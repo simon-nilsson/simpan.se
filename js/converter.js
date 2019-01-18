bbc70940b1a9771f6fdb694faaf61cfaeaf9c9c4
Simon <simon__n@hotmail.com>
2019-01-18 16:25:07 +0100

    Updates

---
+++ js/converter.js
@@ -0,0 +1,26 @@
+﻿function BinaryInput() {
+    var value = document.getElementById("inputvalue");
+    document.getElementById("returndata").innerHTML = "Binary " + value.value;
+}
+
+function DecimalInput() {
+    var value = document.getElementById("inputvalue");
+    document.getElementById("returndata").innerHTML = "Decimal " + value.value;
+}
+
+function HexInput() {
+    var value = document.getElementById("inputvalue");
+    document.getElementById("returndata").innerHTML = "Hex " + value.value;
+}
+
+function OctalInput() {
+    var value = document.getElementById("inputvalue");
+    document.getElementById("returndata").innerHTML = "Octal " + value.value;
+}
+
+
+document.getElementById("binbutton").onclick = BinaryInput;
+document.getElementById("decbutton").onclick = DecimalInput;
+document.getElementById("hexbutton").onclick = HexInput;
+document.getElementById("octbutton").onclick = OctalInput;
+
