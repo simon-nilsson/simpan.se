6ce7b9315302918ec3e5f7993bb55cd9c1250041
Simon <simon__n@hotmail.com>
2019-02-14 14:10:18 +0100

    Updates

--- Binary Conversion/js/converter.js
+++ Binary Conversion/js/converter.js
@@ -1,21 +1,39 @@
 ﻿function BinaryInput() {
-    var value = document.getElementById("inputvalue");
-    document.getElementById("returndata").innerHTML = "Binary " + value.value;
+    var value = document.getElementById("binvalue");
+    document.getElementById("binreturn").innerHTML = "Binary " + value.value;
+    document.getElementById("decreturn").innerHTML = "Decimal " + value.value;
+    document.getElementById("hexreturn").innerHTML = "Hex " + value.value;
+    document.getElementById("octreturn").innerHTML = "Octal " + value.value;
 }
 
 function DecimalInput() {
-    var value = document.getElementById("inputvalue");
-    document.getElementById("returndata").innerHTML = "Decimal " + value.value;
+    var value = document.getElementById("decvalue");
+    document.getElementById("binreturn").innerHTML = "Binary " + value.value;
+    document.getElementById("decreturn").innerHTML = "Decimal " + value.value;
+    document.getElementById("hexreturn").innerHTML = "Hex " + value.value;
+    document.getElementById("octreturn").innerHTML = "Octal " + value.value;
 }
 
 function HexInput() {
-    var value = document.getElementById("inputvalue");
-    document.getElementById("returndata").innerHTML = "Hex " + value.value;
+    var value = document.getElementById("hexvalue");
+    document.getElementById("binreturn").innerHTML = "Binary " + value.value;
+    document.getElementById("decreturn").innerHTML = "Decimal " + value.value;
+    document.getElementById("hexreturn").innerHTML = "Hex " + value.value;
+    document.getElementById("octreturn").innerHTML = "Octal " + value.value;
 }
 
 function OctalInput() {
-    var value = document.getElementById("inputvalue");
-    document.getElementById("returndata").innerHTML = "Octal " + value.value;
+    var value = document.getElementById("octvalue");
+    document.getElementById("binreturn").innerHTML = "Binary " + value.value;
+    document.getElementById("decreturn").innerHTML = "Decimal " + value.value;
+    document.getElementById("hexreturn").innerHTML = "Hex " + value.value;
+    document.getElementById("octreturn").innerHTML = "Octal " + value.value;
+}
+
+function decimalToHex(d) {
+    var hex = Number(d).toString(16);
+    hex = "000000".substr(0, 6 - hex.length) + hex;
+    return hex;
 }
 
 
@@ -23,4 +41,3 @@ document.getElementById("binbutton").onclick = BinaryInput;
 document.getElementById("decbutton").onclick = DecimalInput;
 document.getElementById("hexbutton").onclick = HexInput;
 document.getElementById("octbutton").onclick = OctalInput;
-
